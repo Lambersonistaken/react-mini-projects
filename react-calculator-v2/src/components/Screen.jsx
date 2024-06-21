e5cebe5b9eddfa16bd1da7092de15622af314eb0
Ismail Emir Lambacioglu <ismail.lambacioglu@ogr.sakarya.edu.tr>
2024-06-21 03:04:14 +0300

    added react-textfit

--- react-calculator-v2/src/components/Screen.jsx
+++ react-calculator-v2/src/components/Screen.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
-
+import React, { useContext } from 'react'
+import { CalcContext } from '../context/CalcContext'
+import {Textfit} from 'react-textfit'
 const Screen = () => {
+
+  const {calc} = useContext(CalcContext)
+
   return (
-    <div className='screen'>
-      <p>035746</p>
-    </div>
+    <Textfit className='screen'>
+      035746
+    </Textfit>
   )
 }
 
